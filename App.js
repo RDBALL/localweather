f4af505ff11f1251ad1fd90402aafdaa96bee7ab
Robert Ball <bobball89@gmail.com>
2022-11-14 21:07:15 +0000

    can get local weather from api based on phone geolocation

--- App.js
+++ App.js
@@ -1,14 +1,60 @@
 import { StatusBar } from 'expo-status-bar';
+import React, { useEffect, useState } from 'react';
 import { StyleSheet, Text, View } from 'react-native';
+import * as Location from 'expo-location'
+import { API_KEY, API_URL } from '@env'
+
+// require('@env').config()
 
 export default function App() {
-  return (
+  const [errorMessage, setErrorMessage] = useState(null)
+  const [currentWeather, setCurrentWeather] = useState(null)
+  const [unitMeasurements, setUnitMeasurements] = useState('imperial')
+
+  useEffect(() => {
+    load()
+  }, [])
+
+  async function load() {
+    try {
+      let { status } = await Location.requestForegroundPermissionsAsync()
+      if (status !== 'granted') {
+        setErrorMessage('Location access needed to retrieve your local weather')
+        return
+      }
+      const location = await Location.getCurrentPositionAsync()
+
+      const { latitude, longitude } = location.coords
+      const apiURL = `${API_URL}lat=${latitude}&lon=${longitude}&units=${unitMeasurements}&appid=${API_KEY}`
+      const response = await fetch(apiURL)
+      const result = await response.json()
+
+      if (response.ok) {
+        setCurrentWeather(result)
+      } else {
+        console.log(result)
+      }
+      // alert(`Latitude : ${latitude}, Longitude: ${longitude}`)
+    } catch (error) {
+      setErrorMessage(error.message)
+    }
+  }
+  if (currentWeather) {
+    const { main: { temp } } = currentWeather
+    return (
+      <View style={styles.container}>
+        <Text style={styles.text}>{temp}</Text>
+        <StatusBar style="auto" />
+      </View>
+    )
+  } else {
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
+      <Text>{errorMessage}</Text>
       <StatusBar style="auto" />
     </View>
-  );
+  }
 }
+
 
 const styles = StyleSheet.create({
   container: {
@@ -17,4 +63,7 @@ const styles = StyleSheet.create({
     alignItems: 'center',
     justifyContent: 'center',
   },
+  text: {
+    fontSize: 50,
+  }
 });
